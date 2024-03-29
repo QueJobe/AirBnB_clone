cc21f70c56eb453f7d0f19c51251271a0f7c7ab3
Blessing <bqsithole22@live.com>
2024-03-29 10:44:37 -0700

    base class task 4

--- models/base_model.py
+++ models/base_model.py
@@ -8,10 +8,20 @@ from datetime import datetime
 
 class BaseModel():
     """ Class BaseModel"""
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        """ Constructor to basemodel class"""
+        if kwargs != {}:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "update_at":
+                    val = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, val)
+                    continue
+                if key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """
